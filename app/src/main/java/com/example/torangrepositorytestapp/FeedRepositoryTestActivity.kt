36125dce87377b85a072ae8e05214d96c163a357
DESKTOP-EJOCFMH\sryang <sarang628@naver.com>
2022-08-29 18:01:10 +0900

    update

---
+++ app/src/main/java/com/example/torangrepositorytestapp/FeedRepositoryTestActivity.kt
@@ -0,0 +1,11 @@
+package com.example.torangrepositorytestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FeedRepositoryTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed_repository_test)
+    }
+}
