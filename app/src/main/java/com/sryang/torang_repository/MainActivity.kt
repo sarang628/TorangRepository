88bb0e8cdaae468c3a502328b78529e67a6ce86a
SRYANG <sarang628@naver.com>
2023-05-29 21:56:58 +0800

    update

--- app/src/main/java/com/sryang/torang_repository/MainActivity.kt
+++ app/src/main/java/com/sryang/torang_repository/MainActivity.kt
@@ -2,11 +2,15 @@ package com.sryang.torang_repository
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 import com.posco.feedscreentestapp.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Text(text = "")
+        }
     }
 }
