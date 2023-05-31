8bf7eee38bac0ac17d16daa7d5d873bd51c3f18b
SRYANG <sarang628@naver.com>
2023-05-31 21:21:45 +0800

    update

--- app/src/main/java/com/sryang/torang_repository/MainActivity.kt
+++ app/src/main/java/com/sryang/torang_repository/MainActivity.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.rememberCoroutineScope
 import com.posco.feedscreentestapp.R
+import com.sryang.torang_repository.data.AppDatabase
+import com.sryang.torang_repository.data.entity.UserEntity
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val db = AppDatabase.getInstance(this)
+        db.LoggedInUserDao().getLoggedInUserEntity()
+        runBlocking {
+            db.userDao().insertAll(listOf(UserEntity(userId = 1)))
+        }
         setContent {
             Text(text = "")
         }
