b7574db9699b63f8876fe3663dacb53c0b7518ec
SRYANG <sarang628@naver.com>
2023-10-05 14:04:01 +0800

    update

--- library/src/main/java/com/sryang/torang_repository/data/RestaurantDetail.kt
+++ library/src/main/java/com/sryang/torang_repository/data/RestaurantDetail.kt
@@ -1,13 +1,13 @@
 package com.sryang.torang_repository.data
 
-import android.graphics.Picture
 import com.google.gson.Gson
 import com.sryang.torang_repository.Menu
+import com.sryang.torang_repository.data.entity.ReviewImageEntity
 import com.sryang.torang_repository.data.remote.response.RemoteRestaurant
 
 data class RestaurantDetail(
     val restaurant: RemoteRestaurant,
-    val pictures: ArrayList<Picture>,
+    val pictures: ArrayList<ReviewImageEntity>,
     val comments: ArrayList<RestaurantComment>,
     val menus: ArrayList<Menu>,
     val restaurantRating: RestaurantRating
