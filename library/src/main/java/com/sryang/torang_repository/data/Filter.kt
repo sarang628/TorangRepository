bb42caf9c0d35fa60e1b27dab6d241cd467c5532
SRYANG <sarang628@naver.com>
2023-10-12 11:25:37 +0800

    update

--- library/src/main/java/com/sryang/torang_repository/data/Filter.kt
+++ library/src/main/java/com/sryang/torang_repository/data/Filter.kt
@@ -4,8 +4,8 @@ package com.sryang.torang_repository.data
 data class Filter(
     var searchType: SearchType = SearchType.AROUND,
     var keyword: String? = null,
-    var distances: Distances? = null,
-    var prices: Prices? = null,
+    var distances: String? = null,
+    var prices: String? = null,
     var restaurantTypes: List<String>? = null,
     var ratings: List<String>? = null,
     var lat: Double? = null,
