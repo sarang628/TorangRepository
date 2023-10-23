bd64a3a34a89ec4e5bbf59b6d72e34e391d5b753
SRYANG <sarang628@naver.com>
2023-10-23 16:24:52 +0800

    update

--- library/src/main/java/com/sryang/torang_repository/data/RemoteAlarm.kt
+++ library/src/main/java/com/sryang/torang_repository/data/RemoteAlarm.kt
@@ -1,13 +1,23 @@
 package com.sryang.torang_repository.data
 
+import com.google.gson.annotations.SerializedName
+import com.sryang.torang_repository.data.remote.response.RemoteUser
+
 data class RemoteAlarm(
+    @SerializedName("alarm_id")
     var alarmId: Int,
+    @SerializedName("user_id")
     var userId: Int,
+    @SerializedName("other_user_id")
     var otherUserId: Int,
     var contents: String,
+    @SerializedName("alarm_type")
     var alarmType: String,
+    @SerializedName("review_id")
     var reviewId: Int,
+    @SerializedName("create_date")
     var createDate: String,
-    var user: User,
-    var otherUser: User
+    var user: RemoteUser,
+    @SerializedName("other_user")
+    var otherUser: RemoteUser
 )
