794f8dfd90e84d0a90462144181b808b06dcaf25
sryang <sarang628@gmail.com>
2024-08-18 16:12:37 +0800

    update

--- library/src/main/java/com/sarang/torang/api/ApiFilter.kt
+++ library/src/main/java/com/sarang/torang/api/ApiFilter.kt
@@ -1,6 +1,7 @@
 package com.sarang.torang.api
 
 import com.sarang.torang.data.remote.response.CityApiModel
+import com.sarang.torang.data.remote.response.NationApiModel
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
@@ -14,5 +15,5 @@ interface ApiFilter {
     suspend fun getCitiesByNationId(@Query("nationId") nationId: Int): List<CityApiModel>
 
     @GET("getNations")
-    suspend fun getNations(): List<CityApiModel>
+    suspend fun getNations(): List<NationApiModel>
 }
