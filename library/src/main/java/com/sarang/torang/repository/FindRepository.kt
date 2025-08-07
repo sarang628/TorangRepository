e4a9ff56f4f289db663d6b708398df69571522e5
sryang <sarang628@gmail.com>
2025-08-07 09:42:45 +0800

    change type restaurants to stateflow in FindRepository

--- library/src/main/java/com/sarang/torang/repository/FindRepository.kt
+++ library/src/main/java/com/sarang/torang/repository/FindRepository.kt
@@ -4,24 +4,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import com.sarang.torang.data.Filter
 import com.sarang.torang.data.remote.response.RestaurantApiModel
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 interface FindRepository {
-    val restaurants: List<RestaurantApiModel>
+    val restaurants: StateFlow<List<RestaurantApiModel>>
     suspend fun search(filter: Filter = Filter())
 }
 
 @Composable
 fun FindRepositoryTest(findRepository: FindRepository) {
     val coroutine = rememberCoroutineScope()
+    val restaurants = findRepository.restaurants.collectAsState().value
     Column {
         Button({
             coroutine.launch { findRepository.search() }
         }) {}
-        Text(findRepository.restaurants.toString())
+        Text(restaurants.toString())
     }
-
 }
