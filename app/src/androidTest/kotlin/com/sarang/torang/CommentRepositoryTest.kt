bc5bb39ee2a94c573255613fcf135de7be12b62a
sryang <sarang628@gmail.com>
2025-08-17 17:18:20 +0800

    add CommentRepositoryTest.kt

---
+++ app/src/androidTest/kotlin/com/sarang/torang/CommentRepositoryTest.kt
@@ -0,0 +1,30 @@
+package com.sarang.torang
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.sarang.torang.repository.LoginRepository
+import com.sarang.torang.repository.comment.CommentRepository
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import javax.inject.Inject
+
+@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
+class CommentRepositoryTest {
+    @get:Rule var hiltRule = HiltAndroidRule(this)
+    @Inject lateinit var commentRepository: CommentRepository
+    @Inject lateinit var loginRepository: LoginRepository
+    @Before fun setUp() = runTest { hiltRule.inject(); loginRepository.encEmailLogin("sry_ang@naver.com", "Torang!234") }
+
+    @Test
+    fun loadRestaurantTest() = runTest {
+        var result = commentRepository.getComment(626)
+
+        assertTrue(result.profilePicUrl.isNotEmpty())
+    }
+}
