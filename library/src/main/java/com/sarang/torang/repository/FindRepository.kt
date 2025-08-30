0ab5cfe4b5076a54de69cf68ec3cbb7899d7f0dc
sryang <sarang628@gmail.com>
2025-08-30 08:50:25 +0800

    refactor : saparate test

--- library/src/main/java/com/sarang/torang/repository/FindRepository.kt
+++ library/src/main/java/com/sarang/torang/repository/FindRepository.kt
@@ -1,15 +1,8 @@
 package com.sarang.torang.repository
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.rememberCoroutineScope
 import com.sarang.torang.data.Restaurant
 import com.sarang.torang.data.remote.response.FilterApiModel
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 interface FindRepository {
     val restaurants: StateFlow<List<Restaurant>>
