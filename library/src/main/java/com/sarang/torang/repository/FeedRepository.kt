a776ad8a68338e42367bf965d118055ed2d78b36
sryang <sarang628@gmail.com>
2025-09-09 15:08:05 +0800

    refactor : arrange code

--- library/src/main/java/com/sarang/torang/repository/FeedRepository.kt
+++ library/src/main/java/com/sarang/torang/repository/FeedRepository.kt
@@ -6,13 +6,13 @@ import kotlinx.coroutines.flow.Flow
 
 interface FeedRepository {
     val feeds: Flow<List<ReviewAndImageEntity>>
-    suspend fun findAll()                                                                       /** 모든 피드 가져오기 */
-    suspend fun findById(reviewId: Int)                     : ReviewAndImageEntity              /** 리뷰 ID로 피드 가져오기 */
-    suspend fun findById(reviewId: Int, count: Int = 20)                                        /** 리뷰 ID 다음 피드 가져오기 */
-    suspend fun findByPage(page: Int)                                                           /** 페이지 단위로 피드 가져오기 */
+    suspend fun findAll()
+    suspend fun findById(reviewId: Int)                     : ReviewAndImageEntity
+    suspend fun findById(reviewId: Int, count: Int = 20)
+    suspend fun findByPage(page: Int)
             fun findMyFeedById(reviewId: Int)               : Flow<List<ReviewAndImageEntity>>  /** 리뷰 ID 기준으로 이전 피드는 위로 다음 피드는 아래로 줄 수 있도록 */
-            fun findByRestaurantId(restaurantId: Int)       : Flow<List<ReviewAndImageEntity>>  /** 음식점 ID로 피드 가져오기 */
+            fun findByRestaurantId(restaurantId: Int)       : Flow<List<ReviewAndImageEntity>>
     suspend fun findAllUserFeedById(reviewId: Int)                                              /** ID에 해당 사용자의 모든 피드 가져오기 */
-    suspend fun deleteAll()                                                                     /** 모든 피드 삭제 */
-    suspend fun deleteById(reviewId: Int)                                                       /** 내 리뷰 삭제 */
+    suspend fun deleteAll()
+    suspend fun deleteById(reviewId: Int)
 }
