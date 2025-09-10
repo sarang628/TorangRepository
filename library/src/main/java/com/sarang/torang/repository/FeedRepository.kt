7f8986a7281c83eae5e302a26a2c16ce23394080
sryang <sarang628@gmail.com>
2025-09-10 10:23:28 +0800

    update feedRepository
    - add restaurantFeedsFlow
    - add findByRestaurantId

--- library/src/main/java/com/sarang/torang/repository/FeedRepository.kt
+++ library/src/main/java/com/sarang/torang/repository/FeedRepository.kt
@@ -5,13 +5,14 @@ import kotlinx.coroutines.flow.Flow
 
 
 interface FeedRepository {
-    val feeds: Flow<List<ReviewAndImageEntity>>
+    val feeds                                               : Flow<List<ReviewAndImageEntity>>
+            fun restaurantFeedsFlow(restaurantId : Int)     : Flow<List<ReviewAndImageEntity>>
     suspend fun findAll()
     suspend fun findById(reviewId: Int)                     : ReviewAndImageEntity
     suspend fun findById(reviewId: Int, count: Int = 20)
     suspend fun findByPage(page: Int)
             fun findMyFeedById(reviewId: Int)               : Flow<List<ReviewAndImageEntity>>  /** 리뷰 ID 기준으로 이전 피드는 위로 다음 피드는 아래로 줄 수 있도록 */
-            fun findByRestaurantId(restaurantId: Int)       : Flow<List<ReviewAndImageEntity>>
+    suspend fun findByRestaurantId(restaurantId: Int)
     suspend fun findAllUserFeedById(reviewId: Int)                                              /** ID에 해당 사용자의 모든 피드 가져오기 */
     suspend fun deleteAll()
     suspend fun deleteById(reviewId: Int)
